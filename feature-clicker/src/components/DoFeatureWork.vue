46fe8fdac590fadb52c7556ddbe65cef53d07456
Jessica Kerr <jessitron@gmail.com>
2020-12-28 18:57:55 -0600

    Extract first component.

---
+++ feature-clicker/src/components/DoFeatureWork.vue
@@ -0,0 +1,35 @@
+<template>
+  <svg>
+    <circle
+      class="feature-work"
+      cx="230"
+      cy="100"
+      stroke="black"
+      fill="orange"
+    />
+    <text x="100" y="100" font-size="20" text-anchor="middle" fill="black">
+      <tspan>Work on</tspan>
+      <tspan x="100" dy="1.2em">features</tspan>
+    </text>
+  </svg>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class DoFeatureWork extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped>
+circle.feature-work {
+  r: 70;
+  transition-property: r;
+  transition-duration: 0.5s;
+}
+circle.feature-work:active {
+  r: 65;
+}
+</style>
--- feature-clicker/src/components/HelloWorld.vue
+++ feature-clicker/src/components/HelloWorld.vue
@@ -2,25 +2,20 @@
   <main class="hello">
     <svg class="yo" width="800" height="400">
       Feature Clicker Goes Here
-      <circle
-        class="feature-work"
-        cx="230"
-        cy="100"
-        stroke="black"
-        fill="orange"
-      />
-      <text x="100" y="100" font-size="20" text-anchor="middle" fill="black">
-        <tspan>Work on</tspan>
-        <tspan x="100" dy="1.2em">features</tspan>
-      </text>
+      <DoFeatureWork />
     </svg>
   </main>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import DoFeatureWork from "./DoFeatureWork.vue";
 
-@Component
+@Component({
+  components: {
+    DoFeatureWork,
+  },
+})
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
 }
@@ -35,15 +30,6 @@ svg.yo {
   margin-top: 20px;
   width: 800px;
   height: 400px;
-}
-
-circle.feature-work {
-  r: 70;
-  transition-property: r;
-  transition-duration: 0.5s;
-}
-circle.feature-work:active {
-  r: 65;
 }
 
 h3 {
