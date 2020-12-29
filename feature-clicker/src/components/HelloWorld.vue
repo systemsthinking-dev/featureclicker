e9962ed7ce0efbe853b8cd19b0308d43e0582be4
Jessica Kerr <jessitron@gmail.com>
2020-12-28 18:31:09 -0600

    There is a circle

--- feature-clicker/src/components/HelloWorld.vue
+++ feature-clicker/src/components/HelloWorld.vue
@@ -1,5 +1,14 @@
 <template>
-  <main class="hello">Feature Clicker Goes Here</main>
+  <main class="hello">
+    Feature Clicker Goes Here
+    <svg class="yo" width="800" height="400">
+      <circle cx="230" cy="100" r="70" stroke="black" fill="orange" />
+      <text x="100" y="100" font-size="20" text-anchor="middle" fill="black">
+        <tspan>Work on</tspan>
+        <tspan x="100" dy="1.2em">features</tspan>
+      </text>
+    </svg>
+  </main>
 </template>
 
 <script lang="ts">
@@ -13,6 +22,14 @@ export default class HelloWorld extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only (wow) -->
 <style scoped>
+svg.yo {
+  border: 5px solid gray;
+  display: block;
+  margin: auto;
+  margin-top: 20px;
+  width: 800px;
+  height: 400px;
+}
 h3 {
   margin: 40px 0 0;
 }
