4e11e8575c50c5d2b60c57142a018a217244bb11
Jessica Kerr <jessitron@gmail.com>
2020-12-29 19:47:16 +0000

    We have a number

--- feature-clicker/src/components/HelloWorld.vue
+++ feature-clicker/src/components/HelloWorld.vue
@@ -4,6 +4,7 @@
     <svg class="yo" width="800" height="400">
       Feature Clicker Goes Here
       <DoFeatureWork :doWork="importantThings.featureWorkDone" />
+      <text x="400" y="200">4</text>
     </svg>
   </main>
 </template>
