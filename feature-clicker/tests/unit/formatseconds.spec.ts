0902835097b9208166b756b9bc97b9f0ad282179
Jessica Kerr <jessitron@gmail.com>
2020-12-30 19:28:19 -0600

    Refactor with TCR

--- feature-clicker/tests/unit/formatseconds.spec.ts
+++ feature-clicker/tests/unit/formatseconds.spec.ts
@@ -6,8 +6,8 @@ function formatSeconds(allTheSeconds: number): string {
   function padTo2Digits(n: number): string { return n < 10 ? "0" + n : "" + n }
   const hours = Math.floor(allTheSeconds / (60 * 60));
   const minutes = Math.floor((allTheSeconds % (60 * 60)) / 60);
-  const onlySeconds = allTheSeconds % 60;
-  const prettySeconds = padTo2Digits(onlySeconds);
+  const seconds = allTheSeconds % 60;
+  const prettySeconds = padTo2Digits(seconds);
   return (hours ? hours + ":" + padTo2Digits(minutes) : minutes) + ":" + prettySeconds;
 }
 
