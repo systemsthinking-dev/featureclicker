dcb2863f4f922cf4bba51ff8268d09cb2ea8a676
Jessica Kerr <jessitron@gmail.com>
2020-12-30 19:16:16 -0600

    A test passes

--- feature-clicker/tests/unit/formatseconds.spec.ts
+++ feature-clicker/tests/unit/formatseconds.spec.ts
@@ -3,11 +3,16 @@
 import { assert, expect } from "chai";
 
 function formatSeconds(seconds: number): string {
-  return "" + seconds;
+  const prettySeconds = seconds < 10 ? "0" + seconds : seconds;
+  return "0:" + prettySeconds;
 }
 
 describe("formatSeconds looks nice", () => {
   it("shows 0:00 for 0", () => {
     expect(formatSeconds(0)).to.be.equal("0:00");
+  })
+
+  it("shows 0:10 when 10 seconds have passed", () => {
+    expect(formatSeconds(10)).to.be.equal("0:10");
   })
 })
