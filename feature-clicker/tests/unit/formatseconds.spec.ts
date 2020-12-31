8b14d1598ab05fcb84a8a9fb3716707bb9ae0da3
Jessica Kerr <jessitron@gmail.com>
2020-12-30 19:28:07 -0600

    Refactor with TCR

--- feature-clicker/tests/unit/formatseconds.spec.ts
+++ feature-clicker/tests/unit/formatseconds.spec.ts
@@ -2,11 +2,11 @@
 //import { formatSeconds } from "@/components/TotalValueCreated.vue";
 import { assert, expect } from "chai";
 
-function formatSeconds(seconds: number): string {
+function formatSeconds(allTheSeconds: number): string {
   function padTo2Digits(n: number): string { return n < 10 ? "0" + n : "" + n }
-  const hours = Math.floor(seconds / (60 * 60));
-  const minutes = Math.floor((seconds % (60 * 60)) / 60);
-  const onlySeconds = seconds % 60;
+  const hours = Math.floor(allTheSeconds / (60 * 60));
+  const minutes = Math.floor((allTheSeconds % (60 * 60)) / 60);
+  const onlySeconds = allTheSeconds % 60;
   const prettySeconds = padTo2Digits(onlySeconds);
   return (hours ? hours + ":" + padTo2Digits(minutes) : minutes) + ":" + prettySeconds;
 }
