1868529ff29fc7468d765d484d6967fd141ebc89
Jessica Kerr <jessitron@gmail.com>
2020-12-30 19:50:59 -0600

    Start timer with first click

--- feature-clicker/src/ImportantFile.ts
+++ feature-clicker/src/ImportantFile.ts
@@ -1,6 +1,6 @@
 
 import { Subject, BehaviorSubject, Observable, timer } from "rxjs";
-import { scan, delay } from "rxjs/operators";
+import { scan, delay, first, mergeMap } from "rxjs/operators";
 
 console.log("Does this happen?");
 
@@ -11,7 +11,7 @@ export class ImportantThings {
   constructor() {
     this.featureWorkDone = new Subject<ClickOnFeatureWork>();
     this.capabilityStock = new BehaviorSubject<number>(0);
-    this.secondsSinceBegin = timer(1000, 1000); // really I want it to start on the first click
+    this.secondsSinceBegin = this.featureWorkDone.pipe(first(), mergeMap(_t => timer(1000, 1000))); // start on the first click
 
     const thing: Observable<number> = this.featureWorkDone.pipe(
       scan(countSoFar => countSoFar + 1, 0),
