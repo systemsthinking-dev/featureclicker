292e64367ee5275fbd1683d9f42328545ccfdc4b
Jessica Kerr <jessitron@gmail.com>
2021-01-03 15:28:32 -0600

    Visual clickability

--- feature-clicker/src/components/SendStatusReport.vue
+++ feature-clicker/src/components/SendStatusReport.vue
@@ -19,8 +19,17 @@ export default class SendStatusReport extends Vue {}
 </script>
 
 <style scoped>
+rect.send-status-report {
+  rx: 0;
+  transition-duration: 0.25s;
+}
+rect.send-status-report:active {
+  fill: purple;
+  rx: 10;
+}
 text {
   user-select: none;
+  pointer-events: none;
 }
 .send-status-report {
   fill: lightcoral;
